3688cde7e1ff5c95af8557d4eda0f21457bfd5f1
noernova <noernova666@gmail.com>
2021-12-02 17:38:13 +0700

    add cors config

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/noernova/tai-eng-dictionaryAPI
 go 1.17
 
 require (
+	github.com/gin-contrib/cors v1.3.1
 	github.com/gin-gonic/gin v1.7.7
 	github.com/jinzhu/gorm v1.9.16
 )
--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 	"os"
 
 	"github.com/gin-gonic/gin"
+  "github.com/gin-contrib/cors"
 	"github.com/noernova/tai-eng-dictionaryAPI/controllers"
 	"github.com/noernova/tai-eng-dictionaryAPI/models"
 )
@@ -26,6 +27,19 @@ func setupRouter() *gin.Engine {
 	gin.SetMode(ginMode)
 
 	router := gin.Default()
+
+  router.SetTrustedProxies(nil)
+
+  router.Use(cors.New(cors.Config{
+		AllowMethods:     []string{"GET"},
+		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "User-Agent", "Referrer", "Host", "Token"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		AllowAllOrigins:  false,
+		AllowOriginFunc:  func(origin string) bool { return true },
+		MaxAge:           86400,
+	}))
+
 	router.GET("/", controllers.Index)
 
 	router.GET("/api/v1/api_key=NO8p3FC4qMrTzx1RUjRXNXWrqlLa8DkDjmRgt7s9rDE=/eng/:text", controllers.Eng_to_Shn)
